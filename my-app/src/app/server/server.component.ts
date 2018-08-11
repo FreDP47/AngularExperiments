56f6057852b81e3fce50bde13869c5af98b9212e
Pratyush <mail@pratyushmaharana.com>
2018-08-11 09:28:15 +0530

    directives and data-binding pracctice

--- my-app/src/app/server/server.component.ts
+++ my-app/src/app/server/server.component.ts
@@ -3,11 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  // styleUrls: ['./server.component.css']
+  styles:[`
+    .offline {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent implements OnInit {
+  serverStatus:String = 'Offline';
+  serverId = 0;
+  serverColor:String = '';
+  constructor() { 
+    this.serverId = Math.random();
+    this.serverStatus = this.serverId > 0.5 ? 'online' : 'offline';
+    //this.serverColor = this.serverId > 0.5 ? 'yellow' : 'red';
+  }
 
-  constructor() { }
+  getServerStatus(){
+    return this.serverColor = this.serverId > 0.5 ? 'yellow' : 'red';
+  }
 
   ngOnInit() {
   }
--- my-app/src/app/servers/servers.component.ts
+++ my-app/src/app/servers/servers.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewUser: boolean = false;
+  serverName = 'Please enter server name';
+  textDisplayed = 'no server was created';
+  serverCreated = false;
+  servers = ['server1', 'server2', 'server3'];
+  constructor() { 
+    setTimeout(() => {this.allowNewUser = true;},3000)
+  }
 
   ngOnInit() {
+  }
+
+  oncCreateServer() {
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+    this.textDisplayed = "server created with name" + this.serverName;
+  }
+
+  onUserInputEnter(event:any){
+    this.serverName = event.target.value;
   }
 
 }
